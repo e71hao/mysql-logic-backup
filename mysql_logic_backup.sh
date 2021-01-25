fef5222fdf6b466b8a14edf758507518cab8a227
xieb <xieb@kh-super.com>
2021-01-25 17:26:59 +0800

    mysql xie logic backup

---
+++ mysql_logic_backup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#author: alice
+#time:2021/1/19
+#function: add crontab job .database logic backup for recovery ,every day and delete dumpfile 30 day ago。create dir $mysql_backup_dir.
+#下面脚本是为了在删除表，删除库，意外删除错误数据情况下快速恢复库的备份。关键脚本是mysqldump+binlog2sql达到恢复任意时间点要求。
+source  /etc/profile  
+source ~/.bash_profile
+
+mysql_instance=mysql_shop
+socket=/tmp/mysql.sock
+password=haha
+port=3306
+user=dump_user
+host=localhost
+mysql_dump=/bin/mysqldump
+mysql_backup_dir=/home/mysql
+remaining_day=
+date22=`date +%Y%m%d-%H%M%S`
+date11=`date '+%F'`
+
+dmpfile=${mysql_backup_dir}/pro-dump-${mysql_instance}-${date22}.sql
+dmpfile_gz=${mysql_backup_dir}/pro-dump-${mysql_instance}-${date22}.sql.gz
+logfile=${mysql_backup_dir}/pro-dump-${mysql_instance}-${date22}.log
+mkdir -p $mysql_backup_dir
+
+
+execute_command="$mysql_dump  --user=$user  --password=$password --host=$host  --port=$port --log-error=$logfile  --triggers --routines --events  --single-transaction   --set-gtid-purged=on  --hex-blob  --default-character-set=utf8mb4 --master-data=2 --all-databases  > ${dmpfile}"
+
+eval $execute_command
+
+if [ $? != 0   ] ; then 
+   echo "`date +'%F %T'` $execute_command " >>$logfile
+   echo "`date +'%F %T'` mysqldump backup database ${mysql_instance} fail !" >>$logfile
+   exit 1
+fi
+  grep -E 'Error|error' $logfile  
+if [ $? = 0 ] ; then 
+   echo "`date +'%F %T'` $execute_command " >>$logfile
+   echo "`date +'%F %T'` mysqldump backup database ${mysql_instance} fail !" >>$logfile
+   exit 1
+else 
+   echo "`date +'%F %T'` $execute_command " >>$logfile
+   echo "`date +'%F %T'`  mysqldump backup database ${mysql_instance} success!" >>$logfile
+fi
+#gzip file
+gzip $dmpfile
+#delete  old backup 
+find $mysql_backup_dir  -maxdepth 1 -mtime +${remaining_day} -type f -name "pro-dump*.sql.gz" -exec rm {} \;
+
---
+++ mysql_logic_backup_create.sh
@@ -0,0 +1,99 @@
+usage_str=' 
+mysql_logic_backup usage :
+  --mysql_dump </you/mysqldump/file>\n
+  --host <host-ip>\n
+  --user <uaername>\n 
+  --password <passwd>\n
+  --port 3306\n 
+  --mysql_backup_dir <backup dir>\n 
+  --remaining_day <interger> 
+'
+getopt_method(){
+ARGS=`getopt -o S:,p:,P:,h:,u:  --long socket:,password:,port:,host:,user:,mysql_dump:,mysql_backup_dir:,remaining_day:,help -n "$0" -- "$@"`
+if [ $? != 0 ];then
+        echo $usage_str
+        exit 1
+fi
+#重新排列参数顺序
+eval set -- "${ARGS}"
+#通过shift和while循环处理参数
+while :
+do
+    case $1 in
+       -S | --socket)
+            socket=$2
+            shift
+            ;;
+        -p | --password)
+            password=$2
+            shift
+            ;;
+       -P | --port)
+            port=$2
+            shift
+            ;;
+       -h | --host)
+            host=$2
+            shift
+            ;;
+       -u | --user)
+            user=$2
+            shift
+            ;;
+        --mysql_dump)
+            mysql_dump=$2
+            shift
+            ;;
+        --mysql_backup_dir)
+            mysql_backup_dir=$2
+            shift
+            ;;
+        --remaining_day)
+            remaining_day=$2
+            shift
+            ;;
+        --help)
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo -e $usage_str ;
+            exit 0
+            ;;
+    esac
+shift
+done
+}
+getopt_method "$@"
+
+
+[ ! $mysql_instance ] &&  mysql_instance=mysql_shop
+[ ! $socket ]  && socket=/tmp/mysql.sock
+[ ! $password ] && password=123456
+[ ! $port ] && port=3306
+[ ! $user ] && user=name
+[ ! $host ] && host=localhost
+[ ! $mysql_dump ] && mysql_dump=/bin/mysqldump
+[ ! $mysql_backup_dir ] && mysql_backup_dir=`dirname $0`/..  &&  mysql_backup_dir=`(cd "$BASEDIR"; pwd)`
+[ ! $remaining_date ] && remaining_day=30
+
+ mkdir -p $mysql_backup_dir
+if [ $? != 0 ] ; then 
+ echo "create dir $mysql_backup_dir fail  " ; exit 1
+fi
+mysql_logic_backup_file=${mysql_backup_dir}/b.sh
+sed -i 's#^user=.*#user='$user'#g'  $mysql_logic_backup_file
+sed -i 's#^password=.*#password='$password'#g'  $mysql_logic_backup_file
+sed -i 's#^port=.*#port='$port'#g'  $mysql_logic_backup_file
+sed -i 's#^host=.*#host='$host'#g'  $mysql_logic_backup_file
+sed -i 's#^socket=.*#socket='$socket'#g'  $mysql_logic_backup_file
+sed -i 's#^mysql_instance=.*#mysql_instance='$mysql_instance'#g'  $mysql_logic_backup_file
+sed -i 's#^mysql_dump=.*#mysql_dump='$mysql_dump'#g'  $mysql_logic_backup_file
+sed -i 's#^mysql_backup_dir=.*#mysql_backup_dir='$mysql_backup_dir'#g'  $mysql_logic_backup_file
+sed -i 's#^remaining_date=.*#remaining_date='$remaining_date'#g'  $mysql_logic_backup_file
+
+crontab -l > confi123i ; echo "7 3  * * * $mysql_logic_backup_file" >> confi123i && crontab confi123i && rm -f confi123i
+
+
